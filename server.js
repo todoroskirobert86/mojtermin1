c478932def50c71cd5418daf4d496a7e74d8cd15
todoroskirobert86 <todoroskirobert86@gmail.com>
2025-07-18 23:05:12 +0200

    Initial commit

---
+++ server.js
@@ -0,0 +1,28 @@
+﻿const axios = require('axios')
+const express = require('express')
+
+const app = express()
+const port = process.env.PORT || 3000
+
+app.get('/', (req, res) => {
+  res.send('raboti')
+})
+
+app.listen(port, () => console.log(`server is running on port ${port}...`))
+
+
+function pingServer() {
+  let url = 'https://mojtermin.onrender.com'
+  let randomNumber = Math.floor(Math.random() * 9 + 1)
+  axios(url, { signal: AbortSignal.timeout(10000) })
+    .then(res => {
+      console.log(res.status, url, '- next in', randomNumber, 'mins')
+      setTimeout(pingServer, 1000 * 60 * randomNumber)
+    })
+    .catch(err => {
+      console.log(err.code, url, ' - next in', 1, 'min')
+      setTimeout(pingServer, 60000)
+    })
+}
+
+pingServer()
